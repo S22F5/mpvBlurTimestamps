487570a9a42d575e039fc5b72318e705b0c28094
S22F5 <s22f5@mailfence.com>
2025-09-13 13:11:16 +0200

    improve timestamp reading and error handling

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 ### Usage
 1. move play.sh or play.ps1 into the same folder as your video.
-2. point the script to your video.
-3. create a timestamps.txt file in the same folder (example provided).
+2. (optional) point the script to your video by changing *.mkv.
+3. create a timestamps.txt file in the same folder (examples provided).
 4. run play.sh or play.ps1.
 
 ### timestamps.txt Format
 - Line 1: Name of video for which timestamps are for.
 - Line 2: Playtime in seconds eg. 5623.720s.
 - Line 3-X: timestamps in HH:MM:SS HH:MM:SS.
-- **! the file must have an empty line in the end !**
--- play.sh
+++ play.sh
@@ -8,32 +8,41 @@ command_string="mpv --lavfi-complex=\"[vid1]"
 video_length=$(mpv --no-video --frames=0 --log-file=/dev/stdout $videoFile | grep -i "+ duration:" | awk '{print $6}')
 
 time_to_seconds() {
-  IFS=: read -r h m s <<<"$1"
-  printf "%d" "$((10#$h * 3600 + 10#$m * 60 + 10#$s))"
+	IFS=: read -r h m s <<<"$1"
+	if [[ -z "$h" || -z "$m" || -z "$s" ]]; then
+		return 1
+	fi
+	printf "%d" "$((10#$h * 3600 + 10#$m * 60 + 10#$s))"
 }
 
-# read timestamps from file
+# read timestamp.txt file
 {
-  read -r timestampfilename
-  read -r expected_playtime
-  if [[ "$expected_playtime" != "$video_length" ]]; then
-    printf "\n[ERROR] Playtime does not match\n"
-    printf "[INFO] %s != %s\n" "$expected_playtime" "$video_length"
-    exit 1
-  fi
-  while read -r start_time end_time; do
-    start_seconds=$(time_to_seconds "$start_time")
-    end_seconds=$(time_to_seconds "$end_time")
+	read -r timestampfilename
+	read -r expected_playtime
+	if [[ "$expected_playtime" != "$video_length" ]]; then
+		printf "\n[ERROR] Playtime does not match\n"
+		printf "[INFO] %s != %s\n" "$expected_playtime" "$video_length"
+		exit 1
+	fi
+	while read -r start_time end_time; do
+		start_seconds=$(time_to_seconds "$start_time")
+		end_seconds=$(time_to_seconds "$end_time")
 
-    if [ "$command_string" == "mpv --lavfi-complex=\"[vid1]" ]; then
-      command_string+="gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
-    else
-      command_string+=",gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
-    fi
-  done
+		if [[ -z "$start_seconds" || -z "$end_seconds" ]]; then
+			continue
+		fi
+
+		if [ "$command_string" == "mpv --lavfi-complex=\"[vid1]" ]; then
+			command_string+="gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
+		else
+			command_string+=",gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
+		fi
+	done
 } <timestamps.txt
 
 command_string+="[vo]\" --osd-playing-msg='$timestampfilename' $videoFile"
 
+# print command
+printf "COMMAND: %s\n\n" "$command_string"
 # run mpv
 eval "$command_string"
