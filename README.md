0530bdba919c852b6bdb9aed0bb68fcfe9b9149e
S22F5 <s22f5@mailfence.com>
2026-06-21 16:37:17 +0200

    add support for non fullscreen blurs add aspect ratio check add ms to timestamps add command line options add html editor

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # mpvBlurTimestamps
 
-### Usage
-1. move playLinux.sh or playWindows.ps1 into the same folder as your video.
-2. change the wildcard ***.mkv** in the script *(optional)*.
-3. create a timestamps.txt file in the same folder (examples provided).
-4. run playLinux.sh or playWindows.ps1.
+### Usage (Linux/Windows WSL)
+1. move playLinux.sh into the same folder as your video.
+2. create a .blurs file in the same folder (example provided).
+3. use the script on the video to blur.
 
-### timestamps.txt Format
-- Line 1: **Name** of video for which timestamps are for.
-- Line 2: **Playtime** in seconds eg. 5623.720s.
-- Line 3-X: **Timestamps** in HH:MM:SS HH:MM:SS.
+### Command Line Options
+- -v: videofile
+- -b: blurfile
+- -f: ignore errors
+
+### .blurs Format
+- Line 1: **Info** about file.
+- Line 2: **Playtime** in seconds e.g. 5623.720s.
+- Line 3: **Resolution** in format WidthxHeight e.g. 1920x1080.
+- Line 4-X: **Blurs** in HH:MM:SS.MS HH:MM:SS.MS BlurWidth BlurHeight BlurOffsetX BlurOffsetY (when no width, height, offsets provided blur is fullscreen).
--- playLinux.sh
+++ playLinux.sh
@@ -1,48 +1,97 @@
 #!/bin/bash
-
 blurSigma="20"
-blurSteps="5"
-videoFile="*.mkv"
+blurSteps="1"
 
-command_string="mpv --lavfi-complex=\"[vid1]"
-video_length=$(mpv --no-video --frames=0 --log-file=/dev/stdout $videoFile | grep -i "+ duration:" | awk '{print $6}')
+commandString="mpv --lavfi-complex=\"[vid1]"
 
-time_to_seconds() {
-	IFS=: read -r h m s <<<"$1"
-	if [[ -z "$h" || -z "$m" || -z "$s" ]]; then
-		return 1
-	fi
-	printf "%d" "$((10#$h * 3600 + 10#$m * 60 + 10#$s))"
+while getopts v:b:f flag; do
+  case ${flag} in
+  v) videoFile=$OPTARG ;;
+  b) blurFile=$OPTARG ;;
+  f) force=1 ;;
+  ?) echo "default" ;;
+  esac
+done
+
+if [[ -z "$videoFile" ]]; then
+  videoFile="$1"
+fi
+if [[ -z "$blurFile" ]]; then
+  blurFile="*.blurs"
+fi
+
+videoPlaytime=$(mpv --no-video --frames=0 --log-file=/dev/stdout "$videoFile" | grep -i "+ duration:" | awk '{print $6}')
+IFS='x' read -r videoWidth videoHeight <<<"$(mpv --no-video --frames=0 --log-file=/dev/stdout "$videoFile" | grep -i "Video.*x.*fps) " | sed -E 's/.* ([0-9]+x[0-9]+) .*/\1/')"
+videoAspectR=$(echo "$videoWidth / $videoHeight" | bc -l)
+
+timeToSeconds() {
+  IFS=: read -r h m s <<<"$1"
+  if [[ -z "$h" || -z "$m" || -z "$s" ]]; then
+    return 1
+  fi
+  printf "%d" "$((10#$h * 3600 + 10#$m * 60 + 10#$s))"
 }
 
-# read timestamp.txt file
+# parse timestamp file
 {
-	read -r timestampfilename
-	read -r expected_playtime
-	if [[ "$expected_playtime" != "$video_length" ]]; then
-		printf "\n[ERROR] Playtime does not match\n"
-		printf "[INFO] %s != %s\n" "$expected_playtime" "$video_length"
-		exit 1
-	fi
-	while read -r start_time end_time; do
-		start_seconds=$(time_to_seconds "$start_time")
-		end_seconds=$(time_to_seconds "$end_time")
+  read -r timestampFileInfo
+  read -r expectedPlaytime
+  IFS='x' read -r expectedWidth expectedHeight
+  expectedAspectR=$(echo "$expectedWidth / $expectedHeight" | bc -l)
+  scaleMult=$(echo "$videoWidth / $expectedWidth" | bc -l)
+  if [[ "$force" -ne 1 ]]; then
+    if [[ "$expectedPlaytime" != "$videoPlaytime" ]]; then
+      printf "\n[ERROR] Playtime does not match\n"
+      printf "[INFO] %s != %s\n" "$expectedPlaytime" "$videoPlaytime"
+      exit 1
+    fi
+    if [[ "$expectedAspectR" != "$videoAspectR" ]]; then
+      printf "\n[ERROR] Aspect Ratio does not match\n"
+      printf "[INFO] %s != %.2f\n" "$expectedAspectR" "$videoAspectR"
+      exit 1
+    fi
+  fi
+  firstFilter=true
+  while IFS=' |.' read -r bStartTime bStartTimeMs bEndTime bEndTimeMs bSizeX bSizeY bOffsetX bOffsetY; do
+    bStartSeconds=$(timeToSeconds "$bStartTime")
+    bEndSeconds=$(timeToSeconds "$bEndTime")
+    if [[ -z "$bStartSeconds" || -z "$bEndSeconds" ]]; then
+      continue
+    fi
+    if [[ "$bSizeX" -ne 0 ]]; then
+      #areaBlur
+      bSSizeX=$(echo "$bSizeX * $scaleMult" | bc -l)
+      bSSizeY=$(echo "$bSizeY * $scaleMult" | bc -l)
+      bSOffsetX=$(echo "$bOffsetX * $scaleMult" | bc -l)
+      bSOffsetY=$(echo "$bOffsetY * $scaleMult" | bc -l)
+    else
+      #fullscreenBlur
+      bSSizeX=$videoWidth
+      bSSizeY=$videoHeight
+      bSOffsetX=0
+      bSOffsetY=0
+    fi
 
-		if [[ -z "$start_seconds" || -z "$end_seconds" ]]; then
-			continue
-		fi
+    if [ "$firstFilter" = true ]; then
+      blur=1
+      filterString+="[$blur]crop=$bSSizeX:$bSSizeY:$bSOffsetX:$bSOffsetY,gblur=sigma=$blurSigma:steps=${blurSteps}:enable='between(t,$bStartSeconds.$bStartTimeMs,$bEndSeconds.$bEndTimeMs)'[b$blur];[m][b$blur]overlay=$bSOffsetX:$bSOffsetY:enable='between(t,$bStartSeconds.$bStartTimeMs,$bEndSeconds.$bEndTimeMs)'[bo$blur];"
+      firstFilter=false
+    else
+      ((blur = blur + 1))
+      filterString+="[$blur]crop=$bSSizeX:$bSSizeY:$bSOffsetX:$bSOffsetY,gblur=sigma=$blurSigma:steps=${blurSteps}:enable='between(t,$bStartSeconds.$bStartTimeMs,$bEndSeconds.$bEndTimeMs)'[b$blur];[bo$((blur - 1))][b$blur]overlay=$bSOffsetX:$bSOffsetY:enable='between(t,$bStartSeconds.$bStartTimeMs,$bEndSeconds.$bEndTimeMs)'[bo$blur];"
+    fi
+  done
 
-		if [ "$command_string" == "mpv --lavfi-complex=\"[vid1]" ]; then
-			command_string+="gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
-		else
-			command_string+=",gblur=sigma=$blurSigma:steps=$blurSteps:enable='between(t,$start_seconds,$end_seconds)'"
-		fi
-	done
-} <timestamps.txt
+} <$blurFile
 
-command_string+="[vo]\" --osd-playing-msg='$timestampfilename' $videoFile"
+filterString=${filterString%[*}
+filterString+="[vo]"
+outputsString=$(printf '[%d]' $(seq 1 "$blur"))
+((blur = blur + 1))
+commandString="mpv --lavfi-complex=\"[vid1]split=outputs=${blur}[m]$outputsString;$filterString"
+commandString+="\" --osd-playing-msg='$timestampFileInfo' '$videoFile'"
 
 # print command
-printf "COMMAND: %s\n\n" "$command_string"
+printf "[INFO] Command: %s\n\n" "$commandString"
 # run mpv
-eval "$command_string"
+eval "$commandString"
